b2ae920431799190982ca9681a02484a9ff72012
Vitaly <kaliummati@gmail.com>
2021-07-22 19:18:15 +0900

    update

--- server/server.service.js
+++ server/server.service.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 export const addIdService = (id) => {
   const config = JSON.parse(fs.readFileSync('./server/server.config.json', 'utf-8'));
   if (config.ids.indexOf(String(id)) === -1) {
-    config.ids.push(id);
+    config.ids.push(String(id));
     fs.writeFileSync('./server/server.config.json', JSON.stringify(config));
   }
 }
