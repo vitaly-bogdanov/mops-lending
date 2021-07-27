3e0a79ef3543999e2cc3d1ceb03481d1a34dcd97
Vitaly <kaliummati@gmail.com>
2021-07-27 11:39:52 +0900

    add ssl

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+import https from 'https';
+import http from 'http';
+import fs from 'fs';
+
 import { app, bot } from './server/index.js';
-import { serverCli } from './server/index.js';
 
-// export const httpsServer = https.createServer({
-//   key: fs.readFileSync('../cert/key.pem'),
-//   cert: fs.readFileSync('../cert/cert.pem')
-// }, app);
+const key = fs.readFileSync('./cert/www.magnumopus.space.key', 'utf-8');
+const cert = fs.readFileSync('./cert/www.magnumopus.space.crt', 'utf-8')
 
+https.createServer({ key, cert }, app)
+  .listen(4321, () => console.log('Server start!'));
 
-// export const server = http.createServer(app);
+http.createServer(app)
+  .listen(1234);
 
 bot.launch();
-app.listen(1234, () => console.log('Server start!'));
