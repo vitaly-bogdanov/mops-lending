0b326a50cf5c4043331067328c3321e865398878
Vitaly <kaliummati@gmail.com>
2021-07-27 12:03:28 +0900

    fix port

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ import fs from 'fs';
 import { app, bot } from './server/index.js';
 
 const key = fs.readFileSync('./cert/www.magnumopus.space.key', 'utf-8');
-const cert = fs.readFileSync('./cert/www.magnumopus.space.crt', 'utf-8')
+const cert = fs.readFileSync('./cert/www.magnumopus.space.crt', 'utf-8');
 
 https.createServer({ key, cert }, app)
-  .listen(4321, () => console.log('Server start!'));
+  .listen(443, () => console.log('Server start!'));
 
 http.createServer(app)
-  .listen(1234);
+  .listen(80);
 
 bot.launch();
