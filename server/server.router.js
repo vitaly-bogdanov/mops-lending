b3d2e920e946ee53fd58368ecfa4380b567c7d2b
Vitaly <kaliummati@gmail.com>
2021-09-05 09:02:51 +0900

    fix

--- server/server.router.js
+++ server/server.router.js
@@ -10,6 +10,10 @@ router.get('/', (request, response) => {
   response.status(200).sendFile(path.resolve('index.html'));
 });
 
+router.get('/3m4ugkkltecr94xr42hcebjt4dki8i.html', (request, response) => {
+  response.status(200).sendFile(path.resolve('3m4ugkkltecr94xr42hcebjt4dki8i.html'));
+});
+
 router.post('/send', async (request, response) => {
   getAllIdsService().forEach(id => {
     sendFormDataEvent.emit(id, request.body);
